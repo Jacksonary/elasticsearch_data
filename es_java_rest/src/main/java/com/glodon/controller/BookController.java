6692f5b1d0f1f7a340137a532123c11fda98f577
liuwg-a <jacksonary@163.com>
2018-09-19 14:45:54 +0800

    更新ES 6.4使用Rest Client的CRUD

--- es_java_rest/src/main/java/com/glodon/controller/BookController.java
+++ es_java_rest/src/main/java/com/glodon/controller/BookController.java
@@ -1,23 +1,20 @@
 package com.glodon.controller;
 
 import com.glodon.models.Book;
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.RequestLine;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.*;
+import org.json.JSONObject;
+import org.json.JSONString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author liuwg-a
@@ -50,6 +47,7 @@ public class BookController {
 
     /**
      * 同步执行HTTP请求
+     *
      * @return
      * @throws IOException
      */
@@ -59,8 +57,10 @@ public class BookController {
         Request request = new Request("GET", "/");
 //        // 设置其他一些参数比如美化json
 //        request.addParameter("pretty", "true");
+
 //        // 设置请求体
 //        request.setEntity(new NStringEntity("{\"json\":\"text\"}", ContentType.APPLICATION_JSON));
+
 //        // 还可以将其设置为String，默认为ContentType为application/json
 //        request.setJsonEntity("{\"json\":\"text\"}");
 
@@ -85,6 +85,7 @@ public class BookController {
 
     /**
      * 异步执行HTTP请求
+     *
      * @return
      */
     @RequestMapping(value = "/es/asyn", method = RequestMethod.GET)
@@ -134,5 +135,128 @@ public class BookController {
 //            );
 //        }
 //        latch.await();
+    }
+
+    /**
+     * 添加ES对象, Book的ID就是ES中存储的document的ID，所以最好不要为空，自定义生成的ID太浮夸
+     *
+     * @return ResponseEntity
+     * @throws IOException
+     */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<String> add(@RequestBody Book book) throws IOException {
+        // 构造HTTP请求，第一个参数是请求方法，第二个参数是服务器的端点，host默认是http://localhost:9200，
+        // endpoint直接指定为index/type的形式
+        Request request = new Request("POST", new StringBuilder("/book/book/").
+                append(book.getId()).toString());
+        // 设置其他一些参数比如美化json
+        request.addParameter("pretty", "true");
+
+        JSONObject jsonObject = new JSONObject(book);
+        System.out.println(jsonObject.toString());
+        // 设置请求体并指定ContentType，如果不指定默认为APPLICATION_JSON
+        request.setEntity(new NStringEntity(jsonObject.toString()));
+
+        // 发送HTTP请求
+        Response response = client.performRequest(request);
+
+        // 获取响应体, id: AWXvzZYWXWr3RnGSLyhH
+        String responseBody = EntityUtils.toString(response.getEntity());
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    /**
+     * 根据id获取ES对象
+     *
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> getBookById(@PathVariable("id") String id) {
+        Request request = new Request("GET", new StringBuilder("/book/book/").
+                append(id).toString());
+        // 添加json返回优化
+        request.addParameter("pretty", "true");
+        Response response = null;
+        String responseBody = null;
+        try {
+            // 执行HHTP请求
+            response = client.performRequest(request);
+            responseBody = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            return new ResponseEntity<>("can not found the book by your id", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    /**
+     * 根据id更新Book
+     *
+     * @param id
+     * @param book
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateBook(@PathVariable("id") String id, @RequestBody Book book) throws IOException {
+        // 构造HTTP请求
+        Request request = new Request("POST", new StringBuilder("/book/book/").
+                append(id).append("/_update").toString());
+        request.addParameter("pretty", "true");
+
+        // 将数据丢进去，这里一定要外包一层“doc”，否则内部不能识别
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("doc", new JSONObject(book));
+        request.setEntity(new NStringEntity(jsonObject.toString()));
+
+        // 执行HTTP请求
+        Response response = client.performRequest(request);
+
+        // 获取返回的内容
+        String responseBody = EntityUtils.toString(response.getEntity());
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    /**
+     * 使用脚本更新Book
+     * @param id
+     * @param
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/update2/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateBook2(@PathVariable("id") String id, @RequestParam("name") String name) throws IOException {
+        // 构造HTTP请求
+        Request request = new Request("POST", new StringBuilder("/book/book/").
+                append(id).append("/_update").toString());
+        request.addParameter("pretty", "true");
+
+        JSONObject jsonObject = new JSONObject();
+        // 创建脚本语言，如果是字符变量，必须加单引号
+        StringBuilder op1 = new StringBuilder("ctx._source.name=").append("'" + name + "'");
+        jsonObject.put("script", op1);
+
+        request.setEntity(new NStringEntity(jsonObject.toString(), ContentType.APPLICATION_JSON));
+
+        // 执行HTTP请求
+        Response response = client.performRequest(request);
+
+        // 获取返回的内容
+        String responseBody = EntityUtils.toString(response.getEntity());
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteById(@PathVariable("id") String id) throws IOException {
+        Request request = new Request("DELETE", new StringBuilder("/book/book/").append(id).toString());
+        request.addParameter("pretty", "true");
+        // 执行HTTP请求
+        Response response = client.performRequest(request);
+        // 获取结果
+        String responseBody = EntityUtils.toString(response.getEntity());
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 }
